7cc2e0bfbecdd0c63d51e4f244e6b750578e01ab
Shoaib Ansari <mohd.shoaib0118@gmail.com>
2025-07-03 10:07:42 +0530

    change in sold items api endpoint and bind data

--- src/pages/bmg/orders/Orders.js
+++ src/pages/bmg/orders/Orders.js
@@ -19,6 +19,7 @@ const Orders = () => {
     useEffect(() => {
         setTotalPages(Math.ceil(TotalRecords / pageSize));
     }, [TotalRecords, pageSize]);
+
     useEffect(() => {
         dispatch(getOrdersAction({ search: search, limit: pageSize, page: pageIndex }));
     }, [dispatch, pageIndex, pageSize, search]);
